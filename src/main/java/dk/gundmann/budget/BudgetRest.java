9629451f2d6b86edfd72b03b0e3e8304cb585a5c
Tim Gundmann <tim@gundmann.dk>
2018-09-24 08:40:48 +0200

    swagger added

--- src/main/java/dk/gundmann/budget/BudgetRest.java
+++ src/main/java/dk/gundmann/budget/BudgetRest.java
@@ -1,8 +1,13 @@
 package dk.gundmann.budget;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("budget")
+@RestController
 public class BudgetRest {
 
+	@RequestMapping("/budget")
+	public String get() {
+		return "Test";
+	}
 }
